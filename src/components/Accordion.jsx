a719981ed67304e2f746a187bd80e34e2b80e25a
kolamat <matanmidupe004@gmail.com>
2024-04-30 11:29:51 +0100

    On branch main Your branch is ahead of 'origin/main' by 5 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/components/Accordion.jsx
    	modified:   src/components/Data.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,65 @@
 import { Data } from "./Data";
-import styled from "styled-componets";
+import styled from "styled-components";
 import { IconContext } from "react-icons";
 import { FiPlus, FiMinus } from "react-icons/fi";
 
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+`;
+
+const Container = styled.div`
+  postion: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+const Wrap = styled.div`
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+`;
+
 const Accordion = () => {
-  return <div>Accordion</div>;
+  return (
+    <IconContext.Provider value={{ color: "#00FF89", size: "25px" }}>
+      <AccordionSection>
+        <Container>
+          {Data.map((item) => {
+            return (
+              <>
+                <Wrap>
+                  <h1>{item.title}</h1>
+                </Wrap>
+                <p>
+                  {item.answer1}
+                  <input type="checkbox" />
+                </p>
+                <p>
+                  {item.answer2}
+                  <input type="checkbox" />
+                </p>
+              </>
+            );
+          })}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider>
+  );
 };
 
 export default Accordion;
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -3,15 +3,18 @@ export const Data = [
     title: "Do you want me to be your nigga??",
     answer1: "Yes",
     answer2: "No",
+    keys: "components",
   },
   {
     title: "Can I be your Boyfriend and later BoyLover??",
     answer1: "Yes",
     answer2: "No",
+    keys: "components",
   },
   {
     title: "Will you Marry me??",
     answer1: "Yes",
     answer2: "No",
+    keys: "components",
   },
 ];
