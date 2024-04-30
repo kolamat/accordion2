ba36aeb9b8c6b8820126ccad4992ed4a7962bd68
kolamat <matanmidupe004@gmail.com>
2024-04-30 10:28:51 +0100

    On branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,3 +1,8 @@
+import { Data } from "./Data";
+import styled from "styled-componets";
+import { IconContext } from "react-icons";
+import { FiPlus, FiMinus } from "react-icons/fi";
+
 const Accordion = () => {
   return <div>Accordion</div>;
 };
