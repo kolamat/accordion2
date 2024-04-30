50290c668a9c7618a3d0a73a93f53037ef41340b
kolamat <matanmidupe004@gmail.com>
2024-04-30 10:19:01 +0100

    On branch main Your branch is ahead of 'origin/main' by 3 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	deleted:    src/components/Data.js
    	new file:   src/components/Data.jsx

---
+++ src/components/Data.jsx
@@ -0,0 +1,17 @@
+export const Data = [
+  {
+    title: "Do you want me to be your nigga??",
+    answer1: "Yes",
+    answer2: "No",
+  },
+  {
+    title: "Can I be your Boyfriend and later BoyLover??",
+    answer1: "Yes",
+    answer2: "No",
+  },
+  {
+    title: "Will you Marry me??",
+    answer1: "Yes",
+    answer2: "No",
+  },
+];
