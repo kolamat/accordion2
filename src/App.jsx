96c594ab1bdf7841e86c832b2d0835c3824f1609
kolamat <matanmidupe004@gmail.com>
2024-04-30 10:04:04 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx
    	modified:   src/components/Accordion.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from "react";
 import "./App.css";
+import Accordion from "./components/Accordion";
 
 function App() {
   //   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>Kolss</p>
+      <Accordion />
     </>
   );
 }
