7c1e47e887bb15e42ce5c1bb46dcc046d1b74964
kolamat <matanmidupe004@gmail.com>
2024-05-01 23:51:51 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/Accordion.jsx

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -17,46 +17,50 @@ const Accordion = () => {
   };
 
   return (
-    <div className={`pt-4`}>
-      {Data.map((item, index) => {
-        return (
-          <>
-            <button
-              className={`flex justify-between items-center w-full text-center bg-stone-800 text-white pr-3`}
-              onClick={() => toggle(index)}
-              key={index}
-            >
-              <h1
-                key={item.id}
-                className={`p-8 pl-4 transition-all duration-300 font-bold`}
+    <div className={`h-screen pt-8 bg-stone-800`}>
+      <div
+        className={`border-t-[15px] border-b-[15px] border-blue-950 rounded-3xl`}
+      >
+        {Data.map((item, index) => {
+          return (
+            <>
+              <button
+                className={`flex justify-between items-center w-full text-center bg-stone-800 text-white pr-3`}
+                onClick={() => toggle(index)}
+                key={index}
               >
-                {item.title}
-              </h1>
-              <span className={`rotate-180 transition-all duration-300`}>
-                {clicked === index ? <FaMinus /> : <FaPlus />}
-              </span>
-            </button>
-            {clicked === index ? (
-              <div className={`border-slate-400 bg-stone-800 text-slate-400`}>
-                <p
+                <h1
                   key={item.id}
-                  className={`flex justify-between p-3 pb-0 font-semibold text-xl transition-all duration-500`}
+                  className={`p-8 pl-4 transition-all duration-300 font-bold`}
                 >
-                  {item.answer1}
-                  <input type="checkbox" />
-                </p>
-                <p
-                  key={item.id}
-                  className={`flex justify-between p-3 pt-0 font-semibold text-xl transition-all duration-500`}
-                >
-                  {item.answer2}
-                  <input type="checkbox" />
-                </p>
-              </div>
-            ) : null}
-          </>
-        );
-      })}
+                  {item.title}
+                </h1>
+                <span className={`rotate-180 transition-all duration-300`}>
+                  {clicked === index ? <FaMinus /> : <FaPlus />}
+                </span>
+              </button>
+              {clicked === index ? (
+                <div className={`bg-blue-950 text-slate-400`}>
+                  <p
+                    key={item.id}
+                    className={`flex justify-between p-3 pb-0 font-semibold text-xl transition-all duration-500`}
+                  >
+                    {item.answer1}
+                    <input type="checkbox" />
+                  </p>
+                  <p
+                    key={item.id}
+                    className={`flex justify-between p-3 pt-0 font-semibold text-xl transition-all duration-500`}
+                  >
+                    {item.answer2}
+                    <input type="checkbox" />
+                  </p>
+                </div>
+              ) : null}
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
